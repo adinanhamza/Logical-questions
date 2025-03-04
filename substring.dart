79bd2de96703274d69d266ad44ff862846c65078
adinanhamza <adinanck4557@gmail.com>
2025-03-04 12:42:56 +0530

    subtringcount

---
+++ substring.dart
@@ -0,0 +1,47 @@
+void main(List<String> args) {
+//   Given an array of strings patterns and a string word, return the number of strings in patterns that exist as a substring in word.
+
+// A substring is a contiguous sequence of characters within a string.
+
+ 
+
+// Example 1:
+
+// Input: patterns = ["a","abc","bc","d"], word = "abc"
+// Output: 3
+// Explanation:
+// - "a" appears as a substring in "abc".
+// - "abc" appears as a substring in "abc".
+// - "bc" appears as a substring in "abc".
+// - "d" does not appear as a substring in "abc".
+// 3 of the strings in patterns appear as a substring in word.
+// Example 2:
+
+// Input: patterns = ["a","b","c"], word = "aaaaabbbbb"
+// Output: 2
+// Explanation:
+// - "a" appears as a substring in "aaaaabbbbb".
+// - "b" appears as a substring in "aaaaabbbbb".
+// - "c" does not appear as a substring in "aaaaabbbbb".
+// 2 of the strings in patterns appear as a substring in word.
+
+
+List patterns = ["a","abc","bc","d"];
+String word = "abc";
+
+int substring(patterns,word){
+List lword = word.split('');
+int count = 0;
+print(lword);
+
+
+for(var i=0;i<patterns.length;i++){
+if(word.contains(patterns[i])){
+count++;
+}
+}
+}
+
+int result =  substring(patterns,word);
+print(result);
+}
