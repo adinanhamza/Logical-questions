b13de2993eda424842c91fa1eab9fe8ffffc372e
adinanhamza <adinanck4557@gmail.com>
2025-03-04 13:48:40 +0530

    deleted some files

--- question2129.dart
+++ question2129.dart
@@ -1,19 +1,19 @@
-2129. Capitalize the Title
+// 2129. Capitalize the Title
 
-You are given a string title consisting of one or more words separated by a single space, where each word consists of English letters. Capitalize the string by changing the capitalization of each word such that:
+// You are given a string title consisting of one or more words separated by a single space, where each word consists of English letters. Capitalize the string by changing the capitalization of each word such that:
 
-If the length of the word is 1 or 2 letters, change all letters to lowercase.
-Otherwise, change the first letter to uppercase and the remaining letters to lowercase.
-Return the capitalized title.
+// If the length of the word is 1 or 2 letters, change all letters to lowercase.
+// Otherwise, change the first letter to uppercase and the remaining letters to lowercase.
+// Return the capitalized title.
 
  
 
-Example 1:
+// Example 1:
 
-Input: title = "capiTalIze tHe titLe"
-Output: "Capitalize The Title"
-Explanation:
-Since all the words have a length of at least 3, the first letter of each word is uppercase, and the remaining letters are lowercase.
+// Input: title = "capiTalIze tHe titLe"
+// Output: "Capitalize The Title"
+// Explanation:
+// Since all the words have a length of at least 3, the first letter of each word is uppercase, and the remaining letters are lowercase.
 
 //answer
 
--- question412.dart
+++ question412.dart
@@ -1,17 +1,17 @@
-412. Fizz Buzz
+// 412. Fizz Buzz
   
-  Given an integer n, return a string array answer (1-indexed) where:
+//   Given an integer n, return a string array answer (1-indexed) where:
 
-answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
-answer[i] == "Fizz" if i is divisible by 3.
-answer[i] == "Buzz" if i is divisible by 5.
-answer[i] == i (as a string) if none of the above conditions are true.
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
  
 
-Example 1:
+// Example 1:
 
-Input: n = 3
-Output: ["1","2","Fizz"]
+// Input: n = 3
+// Output: ["1","2","Fizz"]
 
 // Answer
 
