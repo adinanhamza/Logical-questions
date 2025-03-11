ece018af71011ea87ffc4a6eca090235329de07b
adinanhamza <adinanck4557@gmail.com>
2025-03-11 20:39:07 +0530

    added question

---
+++ containingstring.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  
+  String s = 'abcabc';
+
+  int containings(s){
+  List spli = s.split('');
+print(spli);
+for(int i=0;i<spli.length;i++){
+if(spli.contains(s)){
+  
+}
+}
+return 0;
+  }
+  containings(s);
+}
